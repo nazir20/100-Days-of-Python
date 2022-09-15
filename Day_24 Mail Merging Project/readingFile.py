185eb2f533203df082481b52d348dc6d429372b7
Nazir Sharifi <66849567+nazir20@users.noreply.github.com>
2022-09-15 16:39:22 +0300

    add files

---
+++ Day_24 Mail Merging Project/readingFile.py
@@ -0,0 +1,28 @@
+# file = open('my_file.txt')
+# contents = file.read()
+# print(contents)
+# file.close()
+#
+#
+#
+# with open('my_file.txt') as file:
+#     contents = file.read()
+#     print(contents)
+#
+#
+# a - write & append to the prev
+# r - read only
+# w - just write
+# with open('my_file.txt', mode='a') as file:
+#     file.write("\nI'm currently living in Turkey")
+#
+#
+#
+# with open('new_file', mode='w') as new_File:
+#     new_File.write('This is a new file created!')
+#
+#
+#
+# with open('/Users/nazirsharifi/Desktop/my_file.txt') as file:
+#     contents = file.read()
+#     print(contents)
